279c182012acbafa029bcf41b5ab6beed34874fa
arafathrumman <arafath.rumman@gmail.com>
2019-10-18 19:28:48 +0600

    Application now terminates early if the number of arguments passed into it is wrong, fix it

--- StudentList.java
+++ StudentList.java
@@ -6,6 +6,10 @@ public class StudentList {
 		{
 
 		/*Check arguments*/
+		if(args.length != 1)
+       {
+           System.out.println("Usage: java StudentList a|r|c|?student1|+word");
+       }
 		if(args[0].equals("a")) 
 		
 		{
